7a5951e38d4ca9ecfdb56ea8420c8782daf3b551
Kejolk <rarvind248@gmail.com>
2026-02-03 18:18:29 -0800

    src fixed

---
+++ Main.java
@@ -0,0 +1,9 @@
+// CSCI 345 Assignment 2
+// Authors: Sukhman Lally, Arvind Ramesh
+// Purpose: Main flow for overall game 
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ player.java
@@ -0,0 +1,20 @@
+public class player {
+    String name;
+    int rank;
+    int money;
+    int credits;
+    Location location;
+    Role currentRole;
+    int rehearsalChips;
+    boolean hasMoved; 
+
+    public static void main(String[] args) {
+        
+    }
+
+    static void move(newLocation, Location) {
+        
+    }
+
+
+}
